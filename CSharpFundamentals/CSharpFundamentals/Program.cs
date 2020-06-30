396d568d172f0932757b1b989a2ced863fe0c5cd
evantilton <evantilton@gmail.com>
2020-06-30 11:56:31 -0500

    ok getting back into this

---
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var John = new Person();
+            John.FirstName = "John";
+            John.LastName = "Smith";
+            John.Introduce();
+        }
+    }
+}
