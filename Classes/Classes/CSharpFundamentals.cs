a7bb6f49b7c129201cc611d9266b5d30743189b2
evantilton <evantilton@gmail.com>
2020-06-30 11:38:55 -0500

    classes

--- Classes/Classes/CSharpFundamentals.cs
+++ Classes/Classes/CSharpFundamentals.cs
@@ -1,8 +1,25 @@
 ﻿using System;
 
-namespace Classes
+namespace CSharpFundamentals
 {
-    public class Class1
+    public class Person
     {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is", FirstName + "" + LastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var John = new Person();
+            John.FirstName = "John";
+            John.LastName = "Smith";
+            John.Introduce();
+        }
     }
 }
