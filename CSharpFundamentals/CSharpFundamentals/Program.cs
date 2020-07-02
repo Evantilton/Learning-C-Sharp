92a0fcec865142b045968677d998b082b9262863
evantilton <evantilton@gmail.com>
2020-07-01 23:15:08 -0500

    arrays demo finished

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -28,6 +28,11 @@ namespace CSharpFundamentals
             Console.WriteLine(flags[0]);
             Console.WriteLine(flags[1]);
             Console.WriteLine(flags[2]);
+
+            var names = new string[3]{"Jack","John","Mary"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
